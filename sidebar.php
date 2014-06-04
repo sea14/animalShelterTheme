9fcf9558f4e6e350add7146f336caea0307d1de2
Sharon <seaustin@live.unc.edu>
2014-06-03 20:23:15 -0400

    ripped out things from sidebar, I think

---
+++ sidebar.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The Sidebar containing the main widget area
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+?>
+<div id="secondary">
+	<?php
+		$description = get_bloginfo( 'description', 'display' );
+		if ( ! empty ( $description ) ) :
+	?>
+	<h2 class="site-description"><?php echo esc_html( $description ); ?></h2>
+	<?php endif; ?>
+
+	<?php if ( has_nav_menu( 'secondary' ) ) : ?>
+	<nav role="navigation" class="navigation site-navigation secondary-navigation">
+		<?php wp_nav_menu( array( 'theme_location' => 'secondary' ) ); ?>
+	</nav>
+	<?php endif; ?>
+
+
+	<?php endif; ?>
+</div><!-- #secondary -->
